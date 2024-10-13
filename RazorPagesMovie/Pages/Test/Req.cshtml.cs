6d7dd7c8e1689ca387b217f14dfd7ee40ebaf733
kavindasg <kavindasg@gmail.com>
2024-10-13 23:19:47 +0530

    Third party menu added to side bar

--- RazorPagesMovie/Pages/Test/Req.cshtml.cs
+++ RazorPagesMovie/Pages/Test/Req.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using RazorPagesMovie.Models;
@@ -6,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace RazorPagesMovie.Pages.Test
 {
+    [Authorize]
     [IgnoreAntiforgeryToken(Order = 1001)]
     public class ReqModel : PageModel
     {
